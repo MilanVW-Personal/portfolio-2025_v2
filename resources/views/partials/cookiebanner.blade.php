669adbc2be107038822c723c1cfa6109790a1f5f
MilanVW1 <r0985432@student.thomasmore.be>
2025-05-28 16:53:14 +0200

    start gemaakt aan cookiebanner

---
+++ resources/views/partials/cookiebanner.blade.php
@@ -0,0 +1,15 @@
+<!-- Cookie Consent Banner -->
+<div id="cookieBanner" class="cookie-banner fixed bottom-0 left-0 right-0 bg-[#222] text-white px-[15px] py-[20px] flex flex-wrap justify-between items-center z-[1000]">
+  <div class="cookie-banner-text flex-[1 1 60%] mb-[10px]">
+    <p class="m-0">
+      We use cookies to enhance your experience. By clicking "Accept", you consent to our use of cookies. 
+      Learn more in our 
+      <a href="/privacy-policy.html" target="_blank" class="text-[#00bfff] underline">Privacy Policy</a> and 
+      <a href="/cookie-policy.html" target="_blank" class="text-[#00bfff] underline">Cookie Policy</a>.
+    </p>
+  </div>
+  <div class="cookie-banner-buttons flex-[1 1 35%] flex gap-2.5 justify-end">
+    <button class="px-[10px] py-[15px] rounded-[5px] cursor-pointer" onclick="acceptCookies()">Accept</button>
+    <button class="reject px-[10px] py-[15px] rounded-[5px] cursor-pointer" onclick="rejectCookies()">Reject</button>
+  </div>
+</div>
