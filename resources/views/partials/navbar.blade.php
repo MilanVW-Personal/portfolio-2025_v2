2040bf17653efdf7a49bdc6c56c91a5185fcb74b
MilanVW1 <r0985432@student.thomasmore.be>
2025-05-30 19:46:59 +0200

    facebook en insta links verborgen

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,8 +10,8 @@
     </a>
 
     <div class="social-links text-center">
-      <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+      {{-- <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a> --}}
       <a href="https://www.linkedin.com/in/milan-vw-0b7a34277/" class="linkedin"><i class="bi bi-linkedin"></i></a>
       <a href="https://github.com/MilanVW1" class="github"><i class="bi bi-github"></i></a>
     </div>
