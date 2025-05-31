e2f237a2407c4777414cbe9b1dfc790c32e83b4a
MilanVW1 <r0985432@student.thomasmore.be>
2025-05-31 17:01:08 +0200

    ervoor gezorgd dat cookiebanner enkel bij nieuwe sessie toont ipv bij elke page load

--- public/assets/js/hide-cookiebanner.js
+++ public/assets/js/hide-cookiebanner.js
@@ -5,10 +5,15 @@ const acceptEssentialBtn = document.querySelector(".acceptEssentialKnop")
 const rejectAllBtn = document.querySelector(".rejectAllKnop")
 
 // Fn() voor alle en de essentiële cookies te aanvaarden
+if (!sessionStorage.getItem("cookieBannerHasShown"))
+    cookiebanner.classList.remove("hidden");
+else
+    cookiebanner.classList.add("hidden");
+
 const closeCookieBanner = () => {
-    cookiebanner.classList.add("hidden")
-    // console.log("test")
-}
+    cookiebanner.classList.add("hidden");
+    sessionStorage.setItem("cookieBannerHasShown", "true");
+};
 
 // Events op buttons
 acceptAllBtn?.addEventListener("click", closeCookieBanner);
