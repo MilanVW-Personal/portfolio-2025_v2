a55e5b6d4f573506f3758423173d87e8928c1e6e
MilanVW-Personal <milna.van.winkel@gmail.com>
2026-03-17 19:01:10 +0100

    files van eerste versie van portfolio verwijderd om overzichtelijkheid in file-structuur te behouden.

---
+++ lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "CV" => "CV / Resume",
+    "met degelijke studieresulaten",
+    "Rijbewijs B" => "Driving License B",
+    "voorlopig rijbewijs" => "learner's permit"
+];
--- resources/views/components/cv-sections/skills.blade.php
+++
@@ -1,4 +0,0 @@
-<section>
-    <h3 class="text-xl font-semibold border-b pb-1 mb-4">Aangeleerde skills binnen opleiding</h3>
-    @include("components/cv-sections/skills/carousel-skills")
-</section>
